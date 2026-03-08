512d780eaf3f522bee9e17bbe7adc0789a97d11a
Bappy Ahmmed <itznobita958@gmail.com>
2026-03-08 16:32:18 +0600

    dom references and api

---
+++ dashboard.js
@@ -0,0 +1,9 @@
+const API_BASE = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+
+//DOM references
+const issueList   = document.getElementById("issueList")
+const issueCount  = document.getElementById("issueCount")
+const searchInput = document.getElementById("searchInput")
+const searchBtn   = document.getElementById("searchBtn")
+const tabs        = document.querySelectorAll(".tab")
+
